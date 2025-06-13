bb467958d98df2b7e879c7b65604d9b0f1395411
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-06-13 15:31:56 +0530

    .

---
+++ 04 .Flowcharts & Pseudocodes/sumofTwoNumbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class sumofTwoNumbers
+{
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+
+    System.out.println("Enter First Number:");
+    int a=sc.nextInt();
+
+    System.out.println("Enter Second Number:");
+    int b=sc.nextInt();
+
+    int c=a+b;
+
+    System.out.println("Addition of Two Numbers:"+c);
+}
+}
